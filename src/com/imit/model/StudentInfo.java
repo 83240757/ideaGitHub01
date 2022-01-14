92785e1f33c6d52d9b1964b140d9721766c801ce
lingtian <83240757@qq.com>
2022-01-14 23:47:31 +0800

    Initial commit

---
+++ src/com/imit/model/StudentInfo.java
@@ -0,0 +1,14 @@
+package com.imit.model;
+
+/**
+ * description: StudenInfo <br>
+ * date: 2022/1/14 23:44 <br>
+ * author: anthony <br>
+ * version: 1.0 <br>
+ */
+public class StudentInfo {
+    //程序入口
+    public static void main(String[] args) {
+        System.out.println("学生信息");
+    }
+}
